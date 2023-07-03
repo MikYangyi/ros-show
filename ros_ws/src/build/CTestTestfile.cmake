de3738ddfc40b7b5f4e5264fe1a93a318f92b50a
Mikyang <2772539094@qq.com>
2023-07-03 17:11:41 +0800

    ros数据展示
    利用松灵小车上录制的bag包数据（从QQ群下载），编写软件，展示ROS系统中的各类数据，具体包括：
    用命令行窗口显示小车的IMU和里程计（odometry）数据;
    用图形界面显示颜色相机和深度相机的数据（利用OpenCV库）;
    用图形界面显示激光雷达的点云数据（利用PCL库）;
    自行选择一种高级算法（例如语义分割、三维重建、导航定位（SLAM）等），实现该算法（可以直接利用第三方库），将其集成到系统中。

---
+++ ros_ws/src/build/CTestTestfile.cmake
@@ -0,0 +1,11 @@
+# CMake generated Testfile for 
+# Source directory: /home/yangyihang/Desktop/ros_ws/src
+# Build directory: /home/yangyihang/Desktop/ros_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("task4")
+subdirs("task1")
+subdirs("task2")
+subdirs("task3")
